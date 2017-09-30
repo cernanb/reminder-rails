e6f7fd0285e59034b3cc801bb6a88977127116ce
Cernan Bernardo <cernanb@gmail.com>
2017-09-30 00:20:51 -0700

    Add child_params to ChildrenController

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,4 +4,13 @@ class ChildrenController < ApplicationController
     @child = Child.new
   end
 
+  def create
+
+  end
+
+  private
+    def child_params
+      params.require(:child).permit(:dob, :first_name, :last_name, :gender)
+    end
+
 end
