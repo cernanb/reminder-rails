7bd8bf10b178ccce8de5bd95d10a7c969455ae70
Cernan Bernardo <cernanb@gmail.com>
2017-09-30 22:42:30 -0700

    Build crud actions for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,28 @@
 class EventsController < ApplicationController
+  def index
+    @child = Child.find(params[:child_id])
+    @events = @child.events
+  end
   def new
     @child = Child.find(params[:child_id])
     @event = @child.events.build
   end
+
+  def create
+    @child = Child.find(params[:child_id])
+    @event = @child.events.build(event_params)
+
+    if @event.save
+      redirect_to child_events_path(@child)
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:start_time, :end_time, :name)
+    end
 end
